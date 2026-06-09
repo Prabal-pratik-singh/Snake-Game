7ca5ef4b57db841b81d2ccdcfc4c3fd249c4dc5d
Prabal-pratik-singh <mannkumarsinghprabal1@gmail.com>
2026-06-10 00:01:58 +0530

    Fix audio paths

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 //game constant and variable
 let inputDir ={x: 0,y: 0};
 let board = document.getElementById("board");
-let foodSound = new Audio('/food.mp3');
-let gameOverSound = new Audio('/gameover.mp3');
-let moveSound = new Audio('/move.mp3');
-let musicSound = new Audio('/music.mp3');
+let foodSound = new Audio('./food.mp3');
+let gameOverSound = new Audio('./gameover.mp3');
+let moveSound = new Audio('./move.mp3');
+let musicSound = new Audio('./music.mp3');
 let speed = 8;
 let score = 0;
 let lastPaintTime = 0;
